9af44e3949097d6d894f65da537eb55a7170fa77
Emilixipixi <carlosemilio270798@gmail.com>
2026-05-28 23:08:36 -0500

    CEMN-Update Funcionando

---
+++ tallerjdbc/src/main/java/com/krakedev/jdbc/UpdateVehiculo.java
@@ -0,0 +1,54 @@
+package com.krakedev.jdbc;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class UpdateVehiculo {
+	
+	private static final Logger log = LogManager.getLogger(UpdateVehiculo.class);
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Connection con = null;
+		PreparedStatement ps = null;
+		
+		try {
+			
+		con = Conexion.getConnection();
+		
+		String sql = """
+				UPDATE vehiculos SET precio = ?, disponible = ? 
+				WHERE placa = ?
+				""";
+		
+		ps = con.prepareStatement(sql);
+		
+		ps.setDouble(1, 23500.0);
+		ps.setBoolean(2, false);
+		ps.setString(3, "ABC-1234");
+		
+		int filas = ps.executeUpdate();
+		
+		log.info("Filas actualizadas: " + filas);
+		
+		} catch (Exception e) {
+			
+			log.error("Error al actualizar: " + e.getMessage());
+			
+		} finally {
+			
+			try {
+				con.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+}
