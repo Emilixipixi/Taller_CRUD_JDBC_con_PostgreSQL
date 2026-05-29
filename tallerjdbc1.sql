7679bba3da3aab7a863e662fa1f18044b86ff9b9
Emilixipixi <carlosemilio270798@gmail.com>
2026-05-28 23:02:18 -0500

    CEMN-Proyecto creado

---
+++ tallerjdbc1.sql
@@ -0,0 +1,9 @@
+CREATE TABLE vehiculos(
+  placa VARCHAR(10) PRIMARY KEY,
+  marca VARCHAR(50) NOT NULL,
+  modelo VARCHAR(50) NOT NULL,
+  anio INT NOT NULL,
+  precio double precision NOT NULL,
+  color VARCHAR(30),
+  disponible BOOLEAN NOT NULL
+);
